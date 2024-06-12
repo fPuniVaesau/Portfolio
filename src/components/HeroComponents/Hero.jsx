af1a973de789dba5bdce1686a8c762e72f258da8
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-06-12 14:01:40 -0700

    working on the hero section of the portfolio.

--- src/components/HeroComponents/Hero.jsx
+++ src/components/HeroComponents/Hero.jsx
@@ -1,12 +1,14 @@
 import styles from '../../styles/HeroStyles/Hero.module.css';
+import canoeLogo from '../../images/CanoeLogoWebV2.png'
+
 export default function Hero() {
   return (
-    <div>
-      <div>
-        <img />
+    <div className={styles.heroContainer}>
+      <div className={styles.welcomWrapper}>
+        <img className={styles.logo} src={canoeLogo} />
         <span>welcome.</span>
       </div>
-      <div>
+      <div className={styles.nameRoleWrapper}>
         <h1>Filo Vaesau</h1>
         <h2>Digital Craftsman | Developer</h2>
       </div>
