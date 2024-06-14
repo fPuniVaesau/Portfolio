f2bcd95593d77b572d5ec618ba8c8115fa9230eb
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-06-13 18:48:42 -0700

    working on the about me section of the portfolio.

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,18 +1,18 @@
-import profilePic from "../../images/filovaesau.jpg"
-import style from "../../styles/AboutMeStyles/AboutMe.module.css"
+import profilePic from '../../images/filovaesau.jpg';
+import style from '../../styles/AboutMeStyles/AboutMe.module.css';
 
-export default function AboutMe(){
-  return(
+export default function AboutMe() {
+  return (
     <>
       <div className={style.container}>
-        <img className={style.profilePic} src={profilePic} alt="profile picture" />
-
         <div>
-          <div>
-            
-          </div>
+          <img
+            className={style.profilePic}
+            src={profilePic}
+            alt='profile picture'
+          />
         </div>
       </div>
     </>
-  )
+  );
 }
