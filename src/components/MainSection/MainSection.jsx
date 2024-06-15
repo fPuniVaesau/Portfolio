fca10822e9a31b73dcf64572f5b578fd21d7dd45
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-06-15 12:30:36 -0700

    working on the about me section of the portfolio.

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -12,8 +12,11 @@ export default function MainSection() {
       <div className={style.componentTwo}>
         <AboutMeSection />
       </div>
-      {/* This is the card section */}
-      {/* <div className={style.cardWrapper}></div> */}
+
+      <div>
+        <img className ={style.test} src="https://i.pinimg.com/originals/71/3c/bf/713cbf391b7830d6d50738ebf0b91091.gif" alt="" />
+      </div>
+
     </div>
   );
 }
